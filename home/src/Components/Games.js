c4fca91957e23f392c4552efd609e46e20b026e0
Andrew Makris <amakri386@west-mec.org>
2023-06-24 21:35:12 -0700

    Push

--- home/src/Components/Games.js
+++ home/src/Components/Games.js
@@ -3,10 +3,15 @@ import React from 'react'
 const Games = () => {
   return (
     <div className="body">
-        <h2 className="title"></h2>
+        <h2 className="title">Video Games</h2>
         <hr className='Horo'/>
         <div className='row'>
-            <img className='Icon' src="" alt=''/>
+            <img className='Icon' src="https://i.pinimg.com/564x/50/65/61/5065618666610648d8193bea7ab6e382.jpg" alt=''/>
+            <img className='Icon' src="https://i.pinimg.com/564x/ff/98/ed/ff98ed3b33908bc8ad33cb4de1035762.jpg" alt=''/>
+            <img className='Icon' src="https://i.pinimg.com/564x/2e/85/b5/2e85b52b31b9e654389abe4f0e5b78cc.jpg" alt=''/>
+            <img className='Icon' src="https://wallpaperaccess.com/full/2154752.jpg" alt=''/>
+            <img className='Icon' src="https://w0.peakpx.com/wallpaper/336/3/HD-wallpaper-nintendo-mario-character-mario-kart-mario-kart-8-nintendo-switch-thumbnail.jpg" alt=''/>
+            <img className='Icon' src="https://preview.redd.it/your-guys-thoughts-on-if-jayson-tatum-was-on-the-cover-of-v0-i7s3cbzl3b0b1.png?auto=webp&s=5064eeaa426616de4cf039a51ba984c5a54dcf1f" alt=''/>
         </div>
     </div>
   )
--- home/src/Components/Movies.js
+++ home/src/Components/Movies.js
@@ -3,10 +3,15 @@ import React from 'react'
 const Movies = () => {
   return (
     <div className="body">
-        <h2 className="title"></h2>
+        <h2 className="title">Film Collection</h2>
         <hr className='Horo'/>
         <div className='row'>
-            <img className='Icon' src="" alt=''/>
+          <img className='Icon' src="https://i.pinimg.com/564x/2d/a8/54/2da8543e2914bd755265b3582d3d1e47.jpg" alt=''/>
+            <img className='Icon' src="https://i.pinimg.com/564x/e4/69/23/e469239a7c693996965c1fa85be538d2.jpg" alt=''/>
+            <img className='Icon' src="https://i.pinimg.com/736x/16/a8/f8/16a8f8d789a47695fffcb6c6d4311ec5.jpg" alt=''/>
+            <img className='Icon' src="https://i.pinimg.com/564x/51/26/08/512608d675fd98fca4105f90ab7d6d5c.jpg" alt=''/>
+            <img className='Icon' src="https://i.pinimg.com/564x/20/16/5f/20165fb800ed5c0a3f4aec57732c2eca.jpg" alt=''/>
+            <img className='Icon' src="https://i.pinimg.com/564x/36/1e/04/361e0495be172994d8bc6f55138c4b2c.jpg" alt=''/>
         </div>
     </div>
   )
