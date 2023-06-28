9e666e3288a97fb9f2fbfa9110ada96512757fa1
Andrew Makris <amakri386@west-mec.org>
2023-06-28 09:57:48 -0700

    push

--- home/src/Components/Games.js
+++ home/src/Components/Games.js
@@ -9,9 +9,9 @@ const Games = () => {
             <img className='Icon' src="https://i.pinimg.com/564x/50/65/61/5065618666610648d8193bea7ab6e382.jpg" alt=''/>
             <img className='Icon' src="https://i.pinimg.com/564x/ff/98/ed/ff98ed3b33908bc8ad33cb4de1035762.jpg" alt=''/>
             <img className='Icon' src="https://i.pinimg.com/564x/2e/85/b5/2e85b52b31b9e654389abe4f0e5b78cc.jpg" alt=''/>
-            <img className='Icon' src="https://wallpaperaccess.com/full/2154752.jpg" alt=''/>
+            <img className='Icon' src="https://hoopshype.com/wp-content/uploads/sites/92/2022/07/2k23-booker.jpeg" alt=''/>
             <img className='Icon' src="https://w0.peakpx.com/wallpaper/336/3/HD-wallpaper-nintendo-mario-character-mario-kart-mario-kart-8-nintendo-switch-thumbnail.jpg" alt=''/>
-            <img className='Icon' src="https://preview.redd.it/your-guys-thoughts-on-if-jayson-tatum-was-on-the-cover-of-v0-i7s3cbzl3b0b1.png?auto=webp&s=5064eeaa426616de4cf039a51ba984c5a54dcf1f" alt=''/>
+            <img className='Icon' src="https://image.api.playstation.com/cdn/UP0240/CUSA10518_00/Zgjse2LA3r6ZknG2wN4FDAmUuZXwe09g.png" alt=''/>
         </div>
     </div>
   )
